862fab7d8ab6cc9f74627b14a9128fdc304dd0f9
Jairo Colón <83477127+jairocolondev@users.noreply.github.com>
2024-04-25 01:48:14 -0500

    Creación de la ruta de registro

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { users } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.get("/test", test);
+router.get("/user", users);
 
 export default router;

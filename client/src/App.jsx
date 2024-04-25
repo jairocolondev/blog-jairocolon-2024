dd4017a74d0b6ce8c9ca28a0f807341b441df874
Jairo Colón <83477127+jairocolondev@users.noreply.github.com>
2024-04-24 20:22:13 -0500

    Agregando rutas y primeras configuraciones de React, Talwind y Sass

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import SingUp from "./pages/SingUp";
+import SingIn from "./pages/SingIn";
+import Dashboard from "./pages/Dashboard";
+
 export default function App() {
-  return <div className="text-3xl text-balance">Mi Blog en 2024</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sing-up" element={<SingUp />} />
+        <Route path="/sing-in" element={<SingIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  );
 }
